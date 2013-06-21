5c669dba23f7c9d81f66e8c909aad933f0c26fa2
Joshua Kossoff <kossoff.j@gmail.com>
2013-06-21 18:52:29 -0400

    A little more customer work.

--- migration.sql
+++ migration.sql
@@ -1,4 +1,11 @@
 /*delete from mag_restore_1.customer_entity;*/
 
-insert into mag_restore_1.customer_entity (entity_id, email, is_active)
-	select customers_id, customers_email_address, 1 from theretrofitsource_osc22.customers;
+insert into mag_restore_1.customer_entity (entity_id, email, is_active, website_id, group_id, store_id, entity_type_id)
+       select customers_id, customers_email_address, 1, 1, 1, 1, 1 from theretrofitsource_osc22.customers;
+
+
+insert into mag_restore_1.customer_entity_varchar (entity_type_id, attribute_id, entity_id, value)
+       select 1, 5, customers_id, customers_firstname from theretrofitsource_osc22.customers;
+
+insert into mag_restore_1.customer_entity_varchar (entity_type_id, attribute_id, entity_id, value)
+       select 1, 7, customers_id, customers_lastname from theretrofitsource_osc22.customers;
---
+++ reset-magento.sql
@@ -0,0 +1,4 @@
+set sql_safe_updates=0;
+delete from mag_restore_1.customer_entity;
+delete from mag_restore_1.customer_entity_varchar;
+set sql_safe_updates=1;
