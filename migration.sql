363c2ff91bb662d6654c2f34ac6e070a01cab71d
Joshua Kossoff <kossoff.j@gmail.com>
2013-06-25 16:37:59 -0400

    More address work.

--- migration.sql
+++ migration.sql
@@ -6,6 +6,8 @@ truncate mag_restore_1.customer_entity_varchar;
 truncate mag_restore_1.customer_entity;
 set sql_safe_updates=1;
 
+drop temporary table if exists osc_customer_addresses;
+
 /* Migrate customer core data */
 insert into mag_restore_1.customer_entity (
 		entity_id,
@@ -33,15 +35,78 @@ insert into mag_restore_1.customer_entity_varchar (
 		value)
 	select 1, 7, customers_id, customers_lastname from theretrofitsource_osc22.customers;
 
+create temporary table osc_customer_addresses as (
+	select ab.address_book_id,
+			c.customers_id,
+			ab.entry_company,
+			ab.entry_firstname,
+			ab.entry_lastname,
+			ab.entry_street_address,
+			ab.entry_suburb,
+			ab.entry_postcode,
+			ab.entry_city
+	from theretrofitsource_osc22.address_book as ab
+		join theretrofitsource_osc22.customers as c
+		where ab.customers_id = c.customers_id);
+
 /* Migrate customer address core data */
 insert into mag_restore_1.customer_address_entity (
+		entity_id,
 		parent_id,
 		entity_type_id,
 		attribute_set_id,
 		is_active)
-	select customers_id, 2, 0, 1 from theretrofitsource_osc22.customers;
+	select address_book_id, customers_id, 2, 0, 1 
+	from osc_customer_addresses;
+
+/* Address - First Name */
+insert into mag_restore_1.customer_address_entity_varchar (
+		value,
+		attribute_id,
+		entity_id,
+		entity_type_id)
+	select entry_firstname, 20, address_book_id, 2
+	from osc_customer_addresses;
+
+/* Address - Last Name */
+insert into mag_restore_1.customer_address_entity_varchar (
+		value,
+		attribute_id,
+		entity_id,
+		entity_type_id)
+	select entry_lastname, 22, address_book_id, 2
+	from osc_customer_addresses;
+
+/* Address - Company Name */
+insert into mag_restore_1.customer_address_entity_varchar (
+		value,
+		attribute_id,
+		entity_id,
+		entity_type_id)
+	select entry_company, 24, address_book_id, 2
+	from osc_customer_addresses;
+
+/* Address - Street */
+insert into mag_restore_1.customer_address_entity_text (
+		value,
+		attribute_id,
+		entity_id,
+		entity_type_id)
+	select concat_ws('\n', entry_street_address, nullif(entry_suburb, "")),
+		25, address_book_id, 2
+	from osc_customer_addresses;
+
+/* Address - City */
+insert into mag_restore_1.customer_address_entity_varchar (
+		value,
+		attribute_id,
+		entity_id,
+		entity_type_id)
+	select entry_city, 26, address_book_id, 2
+	from osc_customer_addresses;
 
 /* Migrate customer telephone number */
+/*
 insert into mag_restore_1.customer_address_entity_varchar (
 		entity_id,
 		value, 
@@ -51,4 +116,5 @@ insert into mag_restore_1.customer_address_entity_varchar (
 	from theretrofitsource_osc22.customers as c
 		join mag_restore_1.customer_address_entity as cae
 		where c.customers_id = cae.parent_id;
-		
+*/
+
